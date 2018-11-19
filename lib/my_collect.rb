d283f63987f2abc810054ad4ba41e80e3fa432a0
Irfan Maknojia <irfan.mak7@gmail.com>
2018-11-19 21:04:21 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,8 @@ def my_collect(array)
   modified_array = []
   i = 0
   while i < array.length
-    yeild(array[i].split(" ").first)
+    modified_array.push(yeild(array[i].split(" ").first))
     i += 1
   end
   modified_array
 end
-
